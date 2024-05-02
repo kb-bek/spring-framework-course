4854a6ca8d2774071b7c5bc26ce7c089ed840f46
Kubanychbek Talipov <talipovkuba555@gmail.com>
2024-05-02 11:54:55 +0600

    Lesson completed: Annotation Qualifier

---
+++ spring-09-Qualifier/src/main/java/org/example/ClassicalMusic.java
@@ -0,0 +1,10 @@
+package org.example;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ClassicalMusic implements Music{
+	@Override
+	public String getSong() {
+		return "Moonlight Sonata";
+	}
+}
---
+++ spring-09-Qualifier/src/main/java/org/example/MusicPlayer.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+@Component
+public class MusicPlayer {
+
+	private Music music1;
+	private Music music2;
+	private Music music3;
+
+	public MusicPlayer(@Qualifier("classicalMusic") Music music1,
+					   @Qualifier("rockMusic") Music music2,
+					   @Qualifier("jazzMusic") Music music3) {
+		this.music1 = music1;
+		this.music2 = music2;
+		this.music3 = music3;
+	}
+
+
+
+	public String playMusic() {
+		return "Playing: " + music1.getSong() + ", " + music2.getSong() + ", " + music3.getSong();
+	}
+}
